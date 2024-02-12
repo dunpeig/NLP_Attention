92c103c439b2fe34377a473f9144eceb46f83245
Dunpei Gan <dunpeigan@Dunpeis-MacBook-Pro.local>
2024-02-12 09:56:41 -0600

    upload new files

--- transformer_multihead.py
+++ transformer_multihead.py
@@ -46,27 +46,16 @@ def subsequent_mask(size):
     # convert 0 to 1 and 1 to 0
     return torch.from_numpy(1- subsequent_mask)
 
-# keep diagonal lower one and right 
-print(np.triu([[1,2,3],[4,5,6],[7,8,9],[10,11,12]], k=-1))
-# keep diagonal and up right 
-print(np.triu([[1,2,3],[4,5,6],[7,8,9],[10,11,12]], k=0))
-# keep diagonal up right only
-print(np.triu([[1,2,3],[4,5,6],[7,8,9],[10,11,12]], k=1))
-
-size = 5
-sm = subsequent_mask(size)
-print("sm:", sm)
-
-plt.figure(figsize=(5,5))
-plt.imshow(subsequent_mask(20)[0])
 
 
 def attention(query, key, value, mask = None, dropout = None):
     d_k = query.size(-1)
+    #
     scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
 
     if mask is not None:
         scores = scores.masked_fill(mask == 0, -1e9)
+
     p_attn = F.softmax(scores, dim = -1)
 
     if dropout is not None:
@@ -74,11 +63,47 @@ def attention(query, key, value, mask = None, dropout = None):
     return torch.matmul(p_attn, value), p_attn
 
 
+def clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, head, embedding_dim, dropout=0.1):
+        super(MultiHeadedAttention, self).__init__()
+
+        assert embedding_dim % head == 0
+
+        self.d_k = embedding_dim // head 
+
+        self.head = head 
+
+        self.embedding_dim = embedding_dim
+
+        self.linears = clones(nn.Linear(embedding_dim, embedding_dim), 4)
+
+        self.attn = None 
+
+        self.dropout = nn.Dropout(p = dropout)
+
+    def forward(self, query, key, value, mask = None):
+
+        if mask is not None:
+
+            mask = mask.unsqueeze(1)
+
+        batch_size = query.size(0)
+
+        query, key, value = [model(x).view(batch_size, -1, self.head, self.d_k).transpose(1,2)
+                             for model, x in zip(self.linears, (query, key, value))]
+        
+        x, self.attn = attention(query, key, value, mask=mask, dropout = self.dropout)
+        x = x.transpose(1,2).contiguous().view(batch_size, -1, self.head * self.d_k)
+        return self.linears[-1](x)
+    
 d_model = 512
 vocab = 1000
 x = Variable(torch.LongTensor([[100,2,421,508],[491,998,1,221]]))
 emb = Embeddings(d_model, vocab)
-
 embr = emb(x)
 d_model = 512
 dropout = 0.1
@@ -87,15 +112,41 @@ x = embr
 
 pe = PositionalEncoding(d_model, dropout, max_len)
 pe_result = pe(x)
-print("pe_result:", pe_result)
+
+head = 8
+embedding_dim = 512
+dropout = 0.2
 
 query = key = value = pe_result 
 mask = Variable(torch.zeros(2,4,4))
-attn, p_attn = attention(query, key, value,mask=mask)
-print("attn: ", attn)
-print(attn.shape)
-print("p_attn:", p_attn)
-print(p_attn.shape)
 
+mha = MultiHeadedAttention(head, embedding_dim, dropout)
+mha_result = mha(query, key, value, mask)
+print(mha_result)
+print(mha_result.shape)
+
+
+
+
+x = torch.randn(4,4)
+print(x.size())
+y = x.view(16)
+print(y.size())
+z = x.view(-1,8)
+print(z.size())
+
+a = torch.randn(1,2,3,4)
+print(a.size())
+# swap the 2nd and 3rd dimension
+b = a.transpose(1,2)
+print(b.size())
+c = a.view(1,3,2,4)
+print(c.size())
+print(torch.equal(b,c))
+
+
+x = torch.randn(2,3)
+print(x)
+print(torch.transpose(x, 0,1))
 
 

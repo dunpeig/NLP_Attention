ade8b29031fc345a074dffede0ba653db7c619a3
Dunpei Gan <dunpeigan@Dunpeis-MBP.lan>
2024-03-05 15:43:03 -0600

    update

--- string_mal.py
+++ string_mal.py
@@ -30,3 +30,96 @@ input_str = input_str.split(' ')
 print(input_str)
 print(' '.join(input_str))
 
+
+
+# Re 
+
+input = 'natural language processing. 12abc789'
+import re 
+pattern = re.compile(r'.')
+print(re.findall(pattern, input))
+
+pattern = re.compile(r'[abc]')
+print(re.findall(pattern, input))
+
+# a to z and A to Z
+pattern = re.compile(r'[a-zA-Z]')
+print(re.findall(pattern, input))
+# not 
+pattern = re.compile(r'[^a-zA-Z]')
+print(re.findall(pattern, input))
+# or condition
+pattern = re.compile(r'[^a-zA-Z]|[0-9]')
+print(re.findall(pattern, input))
+
+# \d is number [0-9] , \D is [^\d] not number, 
+pattern = re.compile(r'\d')
+print(re.findall(pattern, input))
+
+pattern = re.compile(r'\D')
+print(re.findall(pattern, input))
+
+# \w number and letter, \W not number or letter
+
+pattern = re.compile(r'\w')
+print(re.findall(pattern, input))
+
+pattern = re.compile(r'\W')
+print(re.findall(pattern, input))
+
+# space 
+pattern = re.compile(r'\s')
+print(re.findall(pattern, input))
+
+# repete 
+input = 'natural language processing. 12ab3c789'
+
+# zero or more 
+pattern = re.compile(r'\d*')
+print(re.findall(pattern, input))
+#  1 or more 
+pattern = re.compile(r'\d+')
+print(re.findall(pattern, input))
+# 0 or 1
+pattern = re.compile(r'\d?')
+print(re.findall(pattern, input))
+
+# m times 
+pattern = re.compile(r'\d{2}')
+print(re.findall(pattern, input))
+
+# m to n times 
+pattern = re.compile(r'\d{1,2}')
+print(re.findall(pattern, input))
+
+# match and search 
+input = '1natural language processing. 12ab3c789'
+pattern = re.compile(r'\d')
+# match from the begiining, if not macthed at position 0 then false 
+match = re.match(pattern, input)
+print(match.group())
+
+# replace and modify 
+
+pattern = re.compile(r'\d')
+# substitute
+print(re.sub(pattern, 'number',input))
+# also gives how many times replacement happened 
+print(re.subn(pattern, 'number',input))
+
+# split 
+input = 'natural123machinelearning3deeplearning'
+pattern = re.compile(r'\d+')
+print(re.split(pattern, input))
+
+# naming 
+pattern = re.compile(r'(?P<dota>\d+)(?P<lol>\D+)')
+m =re.search(pattern, input)
+print(m.group('dota'))
+print(m.group('lol'))
+
+# 
+input = 'number 222-121-2123'
+pattern = re.compile(r'(\d\d\d-\d\d\d-\d\d\d)')
+m = re.search(pattern, input)
+print(m.groups())
